d47edcf34f65b485fc6c9c58bb043eb8d11f4275
kupidr <kupidr2001@gmail.com>
2022-03-22 18:58:04 +0600

    edit portfolio

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,4 +1,46 @@
 let allItems = document.getElementById("all-items").children;
 
-let totalProject = document.getElementById("total-project");
-totalProject.innerHTML = `(${allItems.length})`;
+// portfolio title and show total projects
+let totalProjectLength = document.getElementById("total-project");
+totalProjectLength.innerHTML = `(${allItems.length - 12})`; //update 12 and count all-items last empty <div></div>
+
+// filter portfolio
+let filter_btn = document.querySelector(".category-btn").children;
+let parent = document.getElementById("all-items").children;
+for (let i = 0; i < filter_btn.length; i++) {
+  filter_btn[i].addEventListener("click", function () {
+    for (let j = 0; j < filter_btn.length; j++) {
+      filter_btn[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    let target = this.getAttribute("data-target");
+    for (let k = 0; k < parent.length; k++) {
+      parent[k].style.display = "none";
+      if (target == parent[k].getAttribute("data-id")) {
+        parent[k].style.display = "block";
+      }
+      if (target == "all") {
+        parent[k].style.display = "block";
+      }
+    }
+  });
+}
+
+// show more item here
+let showMore = document.querySelector(".show-portfoilo button");
+for (let i = 0; i < allItems.length; i++) {
+  if (i < 12) {
+    allItems[i].style.display = "block";
+  }
+}
+currentItem = 12;
+showMore.onclick = () => {
+  let boxes = [...document.querySelectorAll("#all-items > div")];
+  for (let i = currentItem; i < currentItem + 12; i++) {
+    boxes[i].style.display = "block";
+  }
+  currentItem += 12;
+  if (currentItem >= boxes.length) {
+    showMore.style.display = "none";
+  }
+};
